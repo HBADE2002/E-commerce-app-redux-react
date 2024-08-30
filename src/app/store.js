7bd9b545bfbc818096cec592c631d36bdb32a31f
Hrishikesh Bade <hrishikeshbade007@gmail.com>
2024-08-30 11:41:01 +0530

    small changes

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,18 @@
-//redux store 
-// We set up the Redux store in store.js, 
-// incorporating our cart reducer.
-
+// Import the configureStore function from Redux Toolkit
+// This function simplifies the store setup process
 import { configureStore } from '@reduxjs/toolkit';
+
+// Import the cart reducer from the cartSlice file
 import cartReducer from '../features/cartSlice';
 
+// Create and export the Redux store
 export const store = configureStore({
+  // The reducer object is where we define all the slices of our application state
   reducer: {
+    // Here, we're adding our cartReducer under the key 'cart'
+    // This means our cart state will be available at state.cart in our application
     cart: cartReducer,
   },
+  // Note: configureStore automatically sets up some middleware,
+  // including Redux Thunk for async actions and development tools
 });
--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -1,29 +1,40 @@
-// created a new file cartSlice.js to define
-// our Redux slice for the cart.
-// This includes actions for adding
-// and removing items from the cart.
-
+// Import the createSlice function from Redux Toolkit
 import { createSlice } from "@reduxjs/toolkit";
 
+// Create a slice for the cart state
 const cartSlice = createSlice({
+  // Name of the slice, used in action types
   name: "cart",
+  // Initial state of the slice
   initialState: {
-    items: [],
-    itemCount: 0,
+    items: [],    // Array to store cart items
+    itemCount: 0, // Counter for total items in cart
   },
+  // Reducer functions to handle state changes
   reducers: {
+    // Action to add an item to the cart
     addToCart: (state, action) => {
-      state.items.push(action.payload);
-      state.itemCount += 1;
+      state.items.push(action.payload); // Add new item to items array
+      state.itemCount += 1;             // Increment item count
     },
+    // Action to remove an item from the cart
     removeFromCart: (state, action) => {
+      // Filter out the item with the matching id
       state.items = state.items.filter((item) => item.id !== action.payload.id);
+      // Update item count to match the new number of items
       state.itemCount = state.items.length;
     },
   },
 });
 
+// Export the action creators
 export const { addToCart, removeFromCart } = cartSlice.actions;
+
+// Selector to get cart items from the state
 export const selectCartItems = (state) => state.cart.items;
+
+// Selector to get cart item count from the state
 export const selectCartItemCount = (state) => state.cart.itemCount;
+
+// Export the reducer function
 export default cartSlice.reducer;
